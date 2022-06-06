e26e6cc74165b86dbbf2be5f6dd867e96dd8d00a
lucastimho <lucas.t.ho.lh@gmail.com>
2022-06-06 12:41:33 -0500

    adds ejs package

--- app.js
+++ app.js
@@ -3,8 +3,16 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get("/", function(req, res) {
-    res.send("Hello World");
+    var today = new Date();
+
+    if (today.getDay() === 6 || today.getDay() === 0) {
+        res.send("<h1>Yay it's the weekend!</h1>");
+    } else {
+        res.send("<h1>Boo! Work.</h1>");
+    }
 })
 
 app.listen(3000, function() {
