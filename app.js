a922a0da0d1b96ec7ba33b649b5aafd8c7f6af7e
lucastimho <lucas.t.ho.lh@gmail.com>
2022-06-06 13:04:49 -0500

    adds switch function to retrieve current day code for rendering

--- app.js
+++ app.js
@@ -7,12 +7,35 @@ app.set('view engine', 'ejs');
 
 app.get("/", function(req, res) {
     var today = new Date();
-
-    if (today.getDay() === 6 || today.getDay() === 0) {
-        res.send("<h1>Yay it's the weekend!</h1>");
-    } else {
-        res.send("<h1>Boo! Work.</h1>");
+    var currentDay = today.getDay();
+    var day = "";
+    
+    switch (currentDay) {
+        case 0:
+          day = "Sunday"
+          break
+        case 1:
+            day = "Monday"
+            break
+        case 2:
+            day = "Tuesday"
+            break
+        case 3:
+            day = "Wednesday"
+            break
+        case 4:
+            day = "Thursday"
+            break
+        case 5:
+            day = "Friday"
+            break
+        case 6:
+            day = "Saturday"
+            break
+        default:
+            console.log("Error: current day is equal to: " + currentDay);
     }
+    res.render("list", {kindOfDay: day})
 })
 
 app.listen(3000, function() {
