50e08b4dd5c19d3309564b5c135f5cbe37391e6c
lucastimho <lucas.t.ho.lh@gmail.com>
2022-06-22 19:41:08 -0500

    adds mongoose db and model

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose")
 const date = require(__dirname + "/date.js");
 
 const app = express();
@@ -10,6 +11,14 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true})
+
+const itemsSchema = {
+  name: String
+}
+
+const Item = mongoose.Model("Item", itemsSchema);
 
 const items = ["Buy Food", "Cook Food", "Eat Food"];
 const workItems = [];
