6d37bedadf869ec64cb474f59d39569ac9c56587
lucastimho <lucas.t.ho.lh@gmail.com>
2022-06-22 20:14:27 -0500

    adds conditional statement to prevent continuous db entries

--- views/app.js
+++ views/app.js
@@ -34,18 +34,22 @@ const item3 = new Item ({
 
 const defaultItems = [item1, item2, item3];
 
-Item.insertMany(defaultItems, function(err) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("Successfully saved default items to DB.")
-  }
-})
 
 app.get("/", function(req, res) {
-
+  
   Item.find({}, function(err, foundItems) {
-    res.render("list", {listTitle: "Today", newListItems: foundItems.name});
+    if (foundItems === 0) {
+      Item.insertMany(defaultItems, function(err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Successfully saved default items to DB.")
+        }
+      })
+      res.redirect("/");
+    } else {
+      res.render("list", {listTitle: "Today", newListItems: foundItems.name});
+    }  
   })
 
 const day = date.getDate();
